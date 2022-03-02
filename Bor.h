1c888eaa910104ce7f1cb1c582a83b35749dc2ff
SimayPay <93283693+SimayPay@users.noreply.github.com>
2022-03-02 04:07:14 +0300

    Update Bor.h

--- Bor.h
+++ Bor.h
@@ -35,27 +35,6 @@ void add(const string& s, node* root) {
     cur_v->strings++;
 }
 
-void remove(const string& s, node* root) {
-    node* cur_v = root;
-    node* temp = new node();
-    for (int i = 0; i < s.length(); i++)
-    {
-        char c = s[i];
-        temp = cur_v;
-        cur_v = cur_v->next[c - 'a'];
-
-    }
-    if (cur_v->visit == 1)
-    {
-        delete cur_v;
-        temp->next[s[s.length() - 1] - 'a'] = NULL;
-    }
-    if (cur_v->visit > 1)
-    {
-        cur_v->strings = 0;
-    }
-}
-
 bool has(const string& s, node* root) {
     node* cur_v = root;
 
@@ -63,25 +42,47 @@ bool has(const string& s, node* root) {
         char c = s[i];
         cur_v = cur_v->next[c - 'a'];
         if (cur_v == nullptr) {
-            cout << "Такой строки нет!";
+            cout << "Такой строки нет!" << endl;
             return false;
         }
     }
     if (cur_v->strings == 0)
     {
-        cout << "Такой строки нет!";
+        cout << "Такой строки нет!" << endl;
         return false;
     }
     else
     {
-        cout << "Строка есть!";
+        cout << "Строка есть!" << endl;
         return cur_v->strings > 0;
+    }
+}
+
+void remove(const string& s, node* root) {
+    node* cur_v = root;
+    node* temp = new node();
+    if (has(s, root)) {
+        for (int i = 0; i < s.length(); i++)
+        {
+            char c = s[i];
+            temp = cur_v;
+            cur_v = cur_v->next[c - 'a'];
+            if (cur_v != NULL && cur_v->visit == 1) {
+                delete cur_v;
+                temp->next[c - 'a'] = NULL;
+            }
+        }
+        if (cur_v != NULL && cur_v->visit > 1)
+        {
+            cur_v->strings = 0;
+        }
+        cout << "Строка удалена!" << endl;
     }
 }
 
 void write(node* v, string cur_str) {
     for (int i = 0; i < v->strings; i++) {
-        cout << endl << cur_str << endl;
+        cout << cur_str << endl;
     }
 
     for (int i = 0; i < 26; i++) {
